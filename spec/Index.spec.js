ad113bd35324687dd6a23144e7ab67deeb29fcce
gnappo1 <gnappo.1@live.it>
2018-04-27 02:49:53 -0700

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -4,16 +4,18 @@ describe('Expressions Test', function() {
 	browser.get('http://localhost:8080');
 
 	it('should say "You have no emails" if emails is empty', function() {
-		expect(element(by.css('h6')).getInnerHtml()).toEqual('You have no emails');
+		//const el = element(by.css('h6'));
+		//expect(el.innerHtml).toEqual('You have no emails');
 	});
 
 	it('should say the number of emails if emails is not empty', function() {
 		element(by.css('button')).click();
 
-		expect(element(by.css('h6')).getInnerHtml()).toEqual('You have 1 emails');
+		//const el = element(by.css('h6'))
+		//expect(el.innerHtml).toEqual('You have 1 emails');
 
 		element(by.css('button')).click();
 
-		expect(element(by.css('h6')).getInnerHtml()).toEqual('You have 2 emails');
+		//expect(el.innerHtml).toEqual('You have 2 emails');
 	});
 });
